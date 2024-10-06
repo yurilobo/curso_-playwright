c14028501dbc1845676f44ad6f743cc1f3b7f812
Yuri <yuri.anderson.lobo@gamail.com>
2024-10-06 15:01:42 -0300

    playwrigh test parte 2

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,14 +1,30 @@
 const { test, expect } = require('@playwright/test');
-
-test('basic test', async ({ page }) => {
+ 
+test.beforeEach(async ({ page }) => {
     await page.goto('https://react-redux.realworld.io/#/login')
-    const title = await page.title()
-    expect(title).toBe('Conduit')
-
+ 
+})
+ 
+test('basic test', async ({ page }) => {
+    await expect(page).toHaveTitle('Conduit')
+    
+    expect(await page.screenshot()).toMatchSnapshot('SignIn.png', {threshold: 0.2});
+ 
     await page.fill('input[type = "email"]', 'alanvoigt@yahoo.com.br')
     await page.press('input[type = "email"]', 'Tab')
     await page.type('input[type = "password"]', 'test123')
     await page.click('form >> "Sign in"')
-    const logoText = await page.$eval('.navbar-brand', el => el.innerText)
-    expect(logoText).toBe('conduit')
+    const locator = page.locator('.navbar-brand')
+    await expect(locator).toContainText('conduit', {timeout: 30000});
+})
+ 
+test('login test', async ({ page }) => {
+    await expect(page).toHaveTitle('Conduit')
+    
+    await page.fill('input[type = "email"]', 'alanvoigt@yahoo.com.br')
+    await page.press('input[type = "email"]', 'Tab')
+    await page.type('input[type = "password"]', 'test123')
+    await page.click('form >> "Sign in"')
+    const locator = page.locator('.navbar-brand')
+    await expect(locator).toContainText('conduit', {timeout: 30000});
 })
